ceb35b5e7de4ef4f460d89849438ba0ba4756c48
dhruvesh <Dpatel@outlook.Fr>
2025-05-14 20:57:40 +0200

    intégration des données de comptes

--- my-react-app/src/pages/User/User.jsx
+++ my-react-app/src/pages/User/User.jsx
@@ -95,12 +95,56 @@ function User() {
                     <button type="button" className="cancel-button" onClick={handleCancelClick}>Cancel</button>
                   </div>
               </form>
-
-
-
           </div>
         )}
       </div>
+
+          <h2 className="sr-only">Accounts</h2>
+      {}
+      <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button 
+                className="transaction-button" 
+              >
+                View transactions
+              </button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+              <p className="account-amount">$10,928.42</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button 
+                className="transaction-button"
+                onClick={() => navigate('/transactions')}
+              >
+                View transactions
+              </button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button 
+                className="transaction-button"
+                onClick={() => navigate('/transactions')}
+              >
+                View transactions
+              </button>
+            </div>
+      </section>
     </main>
   );
 }
